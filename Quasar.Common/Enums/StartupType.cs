76c8cd5cc96d6fbe62414689915d87ca33d391de
Amina-farah <113688681+Amina-farah@users.noreply.github.com>
2023-12-24 06:01:35 +0300

    update file

---
+++ Quasar.Common/Enums/StartupType.cs
@@ -0,0 +1,13 @@
+namespace Quasar.Common.Enums
+{
+    public enum StartupType
+    {
+        LocalMachineRun,
+        LocalMachineRunOnce,
+        CurrentUserRun,
+        CurrentUserRunOnce,
+        StartMenu,
+        LocalMachineRunX86,
+        LocalMachineRunOnceX86
+    }
+}
