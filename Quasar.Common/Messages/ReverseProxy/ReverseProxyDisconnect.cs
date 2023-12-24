8c0fff26b373db4080054ff1005c02d098940ac7
Amina-farah <113688681+Amina-farah@users.noreply.github.com>
2023-12-24 06:03:29 +0300

    update file

---
+++ Quasar.Common/Messages/ReverseProxy/ReverseProxyDisconnect.cs
@@ -0,0 +1,11 @@
+using ProtoBuf;
+
+namespace Quasar.Common.Messages.ReverseProxy
+{
+    [ProtoContract]
+    public class ReverseProxyDisconnect : IMessage
+    {
+        [ProtoMember(1)]
+        public int ConnectionId { get; set; }
+    }
+}
